803786be9f6d745fb26c6ba4c2f85c80f8b69c32
nevil-06 <nevilitaliya00@gmail.com>
2025-05-29 11:45:17 -0400

    updated main, req.txt

--- main.py
+++ main.py
@@ -2,18 +2,21 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from scraper.generate_answer import generate_answer
-import traceback
+import traceback, os
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional, List
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI(title="Nestlé Product Answer API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=os.environ.get("FRONTEND_URL"),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    
 )
 
 class QueryRequest(BaseModel):
